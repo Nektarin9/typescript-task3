dac334a9522af2331e33c836b3eeb15857b049ca
Каспаров Владислав Маратович <vlad-kasparov@mail.ru>
2025-01-01 20:07:53 +0300

    start

---
+++ app.ts
@@ -0,0 +1,30 @@
+const COMMENTS_URL = 'https://jsonplaceholder.typicode.com/comments';
+
+
+interface GetDataType {
+  postId: number
+  id: number
+  name: string
+  email: string
+  body: string
+}
+
+
+const getData = async (url : string): Promise<GetDataType[]> => {
+ const response = await fetch(url)
+ return response.json()
+}
+
+getData(COMMENTS_URL)
+  .then(data => {
+   console.log(data.map(({id, email}) => ({id, email})))
+  })
+
+
+/**
+ * ID: 1, Email: Eliseo...
+ * ID: 2, Email: Jayne_Kuhic...
+ * ID: 3, Email: Nikita...
+ * ID: 4, Email: Lew...
+ * ...
+ */
